8130960cd96181b4b5a63ededf9cc85b3e295b63
Philip Rurka <philiprurka@gmail.com>
2016-07-20 13:20:47 -0300

    Save, Delete has been added

--- scripts/app.js
+++ scripts/app.js
@@ -4,22 +4,55 @@
 angular.module('TodoListApp', [])
 
 .directive('newTask', function() {
-    return {
+   return {
         template: '<a href="">+ Add a New Task</a>'
     };
 })
 
 .controller('mainCtrl', function($scope, dataList) {
 
+    /**************** Need For File ****************/
+   //  $scope.inputLabels = [
+   //      {"name": "feed the fishes"},
+   //      {"name": "Drink more coffee"},
+   //      {"name": "Pay for napkins"},
+   //      {"name": "Trip and fall."},
+   //      {"name": "Sing to the Bearded Lady"}
+   //  ]
+    /**************** Need For File ****************/
+
+    /**************** Need For Github ****************/
     dataList.getList(function(response){
         console.log(response.data);
         $scope.inputLabels = response.data;
     })
+    /**************** Need For Github ****************/
+
+   $scope.saveListItem = function(inputLabel) {
+      dataList.saveListItem(inputLabel);
+   };
+
+   $scope.deleteListItem = function(inputLabel, $index) {
+      dataList.deleteListItem(inputLabel);
+      $scope.inputLabels.splice($index, 1);
+   };
 })
 
 .service('dataList', function($http) {
+
+    /**************** Need For Github ****************/
     this.getList = function(callback) {
         $http.get('../Angular-Practice/mock/todo-list.json')
         .then(callback)
+    }
+    /**************** Need For Github ****************/
+
+
+    this.deleteListItem = function(inputLabel) {
+        console.log('The ' + inputLabel.name + ' todo has been deleted.');
+    }
+
+    this.saveListItem = function(inputLabel) {
+        console.log('The ' + inputLabel.name + ' todo has been saved.');
     }
 })
