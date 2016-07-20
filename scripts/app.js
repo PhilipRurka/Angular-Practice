b1db52ef2864a2cadb081ff2f5526f240e15d67a
Philip Rurka <philiprurka@gmail.com>
2016-07-20 11:43:21 -0300

    Test

---
+++ scripts/app.js
@@ -0,0 +1,22 @@
+angular.module('TodoListApp', [])
+
+.directive('newTask', function() {
+    return {
+        template: '<a href="">+ Add a New Task</a>'
+    };
+})
+
+.controller('mainCtrl', function($scope, dataList) {
+
+    dataList.getList(function(response){
+        $scope.inputLabels = response.data;
+    })
+
+})
+
+.service('dataList', function($http) {
+    this.getList = function(callback) {
+        $http.get('mock/todo-list.json')
+        .then(callback)
+    }
+})
