ad4f515325d687933c7f9ca39ef09eee9bd198a9
Philip Rurka <philiprurka@gmail.com>
2016-07-26 15:00:00 -0300

    fff ttt

--- scripts/directives/apiTest.js
+++ scripts/directives/apiTest.js
@@ -2,7 +2,7 @@
 angular.module('apiTesting')
 .directive('api', function() {
    return {
-        template: <h1>Testing</hi>,
+        template: <h1>Testing</h1>,
         controller: 'apiCtrl',
         replace: true
     };
