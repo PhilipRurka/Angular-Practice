69b13c21a8f10043606b750c3c1fa623d7fe9156
Philip Rurka <philiprurka@gmail.com>
2016-07-26 14:29:15 -0300

    fff

--- scripts/controllers/apiCtrl.js
+++ scripts/controllers/apiCtrl.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('TodoListApp')
+angular.module('apiTesting')
 .controller('apiCtrl', function($scope, $http) {
    $scope.list;
    $http.jsonp('https://api.dribbble.com/v1/users/simplebits/followers?page=2').then(function(data) {
